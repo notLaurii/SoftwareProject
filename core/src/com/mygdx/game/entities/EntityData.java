0ad48e7586570e077884278499bc3fbe652667f6
Tyyymeeen <157370559+Tyyymeeen@users.noreply.github.com>
2024-01-22 20:12:11 +0100

    Delete core directory

--- core/src/com/mygdx/game/entities/EntityData.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.entities;
-public class EntityData {
-    public String type;
-    public float x;
-    public float y;
-    public float health;
-    public float attackDamage;
-}
