d55ffb65f6378919b200e7daa656d41a7a074091
notLaurii <136428618+notLaurii@users.noreply.github.com>
2024-02-27 09:16:30 +0100

    Update GameManager.java

--- core/src/com/mygdx/game/management/GameManager.java
+++ core/src/com/mygdx/game/management/GameManager.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.management;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,7 @@ import static com.mygdx.game.management.MyGdxGame.*;
 public class GameManager {
     private int level;
     private int room;
+    private boolean gameRunning = true;
 
     public GameManager(int playerId, int level) {
         this.level=level;
@@ -16,21 +18,23 @@ public class GameManager {
     }
 
     public void update(float deltaTime) {
-            if(levelManager.isEntitiesCreated()&&levelManager.noEnemiesLeft()) {
-                if (level == room) {
-                    System.out.println(level + "; " + room);
-                    setLevel(level + 1);
-                }
-                if (Math.ceil(levelManager.getPlayer().getX())>=(gameMap.getPixelWidth()-levelManager.getPlayer().getWidth()))
-                    if (level == room+1 || room == 0) {
-                        FileHandle folder = Gdx.files.internal("Map/Level" + level);
-                        gameSaver.savePlayerData(levelManager.getPlayer());
-                        gameSaver.saveGameProgress(levelManager.getPlayer());
-                        if (folder.exists())
-                            levelManager.switchLevel();
-                        else {
-                            goToMainRoom();
-                            System.out.println(room + " vs. " + level);
+        if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+            gameRunning=!gameRunning;
+        if(levelManager.isEntitiesCreated()&&levelManager.noEnemiesLeft()) {
+            if (level == room) {
+                System.out.println(level + "; " + room);
+                setLevel(level + 1);
+            }
+            if (Math.ceil(levelManager.getPlayer().getX())>=(gameMap.getPixelWidth()-levelManager.getPlayer().getWidth()))
+                if (level == room+1 || room == 0) {
+                    FileHandle folder = Gdx.files.internal("Map/Level" + level);
+                    gameSaver.savePlayerData(levelManager.getPlayer());
+                    gameSaver.saveGameProgress(levelManager.getPlayer());
+                    if (folder.exists())
+                        levelManager.switchLevel();
+                    else {
+                        goToMainRoom();
+                        System.out.println(room + " vs. " + level);
                     }
                 }
             }
@@ -57,5 +61,12 @@ public class GameManager {
     }
     public void setRoom(int i) {
         room=i;
+    }
+
+    public boolean isGameRunning() {
+        return gameRunning;
+    }
+
+    public void setGameRunning(boolean b) { gameRunning=b;
     }
 }
