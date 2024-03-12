901536aa2e4fd81ed85ec2222aa1bb5a408c2254
notLaurii <136428618+notLaurii@users.noreply.github.com>
2024-03-12 08:54:57 +0100

    Update EntityData.java

--- core/src/com/mygdx/game/entities/EntityData.java
+++ core/src/com/mygdx/game/entities/EntityData.java
@@ -9,6 +9,7 @@ public class EntityData {
     private float x;
     private float y;
     private float maxHealth;
+    private float health;
     private float attackDamage;
     private float speed;
     private float jumpVelocity;
@@ -77,6 +78,13 @@ public class EntityData {
 
     public void setMaxHealth(float maxHealth) {
         this.maxHealth = maxHealth;
+    }
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
     }
 
     public float getY() {
