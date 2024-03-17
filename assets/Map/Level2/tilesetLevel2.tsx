c49410e54c7860d6fa46efe8b67d13439356670c
Malteeeeeee <156671849+Malteeeeeee@users.noreply.github.com>
2024-03-17 13:28:33 +0100

    Add files via upload

---
+++ assets/Map/Level2/tilesetLevel2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tileset" tilewidth="16" tileheight="16" tilecount="60" columns="15">
+ <image source="tilesetLevel2.png" width="240" height="64"/>
+</tileset>
